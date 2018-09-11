72c7324f4e89c61a2d3166e79be32866c46b9f98
Jayson <sontran.hubt@gmail.com>
2018-09-11 12:00:36 +0700

    Design database

---
+++ database/migrations/2018_09_11_034233_create_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_id');
+            $table->integer('member_id');
+            $table->string('role',5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
