755c3b0887eac7bc9f5a061ff315fad738834445
Jayson <sontran.hubt@gmail.com>
2018-09-16 21:20:33 +0700

    Create project

--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    public function storeProject($request)
+    {
+        return $this->saveData($this, $request);
+    }
+
+    public function saveData($project, $request)
+    {
+        $project->name = $request->name;
+        $project->information = $request->information;
+        $project->deadline = $request->deadline;
+        $project->type = $request->type;
+        $project->status = $request->status;
+        $project->save();
+
+        return $project;
+    }
 }
