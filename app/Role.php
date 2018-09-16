e627cc2fa0a58568f63d2e392bbc1e979c746671
Jayson <sontran.hubt@gmail.com>
2018-09-17 00:15:28 +0700

    Delete project

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    public function projects()
+    {
+        return $this->belongsTo('App\Project','project_id','id');
+    }
 }
