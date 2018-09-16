cb9ae8cace2d3745c1b1ebc3e785e987e27446d3
Jayson <sontran.hubt@gmail.com>
2018-09-16 22:05:40 +0700

    Update project

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Project;
+use App\Http\Requests\UpdateProjectRequest;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProjectRequest  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProjectRequest $request, $id)
+    {
+        $project = new Project;
+        $result = $project->updateProject($request, $id);
+
+        return response()->json($result);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    public function updateProject($request, $id)
+    {
+        $project = $this->find($id);
+
+        return $this->saveData($project, $request);
+    }
+
+    public function saveData($project, $request)
+    {
+        $project->name = $request->name;
+        $project->information = $request->information;
+        $project->deadline = $request->deadline;
+        $project->type = $request->type;
+        $project->status = $request->status;
+        $project->save();
+
+        return $project;
+    }
 }
